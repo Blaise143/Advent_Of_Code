e2a51400733eaa257c7de9fbf0377ab953fb0986
Blaise143 <44302302+Blaise143@users.noreply.github.com>
2021-12-01 15:11:01 -0800

    Add files via upload

---
+++ Advent_of_code/advent_of_code_day1.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+# PART 1
+#A function that takes in a vector of inputs and outputs logistic vector
+# of whether or not the depth mesurement increased
+get_next <- function(some_list){
+  some_list > lag(some_list)
+}
+
+data <- Advent_day_1 
+
+## Getting the Number of depth increases
+data %>% 
+  mutate(is_greater = get_next(data) %>% as.data.frame()) %>%
+  filter(is_greater==TRUE | is_greater== FALSE) %>% 
+  pull(is_greater) %>% as_tibble() %>% sum()
+
+##For today's challenge, the output was 1681
+# PART 2
+data %>% 
+  select(V1) %>% 
+  summarize(diff=diff(V1,lag=3)>0) %>% 
+  filter(diff) %>% nrow()
